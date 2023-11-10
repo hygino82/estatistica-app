5432114df98624efab99b2540d3e12efde40cab2
Adroaldo <hygino@msn.com>
2023-11-10 15:21:56 -0300

    Try fix routes 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+/*import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { Outlet } from "react-router-dom";
 
@@ -10,5 +10,32 @@ function App() {
     </div>
   );
 }
+
+export default App;*/
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from 'react-router-dom';
+import { NumericForm } from './pages/NumericForm';
+import { DescriptiveForm } from './pages/DescriptiveForm';
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/estatistica-app/numeric-form">Numeric Form</Link></li>
+          <li><Link to="/estatistica-app">Descriptive Form</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/estatistica-app/numeric-form" element={<NumericForm />} />
+        <Route path="/estatistica-app" element={<DescriptiveForm />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
